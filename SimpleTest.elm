a9921652535ab197c275a7fa79155aa5bca7c67a
Hussein Morsy <hussein@morsy.de>
2016-07-31 13:43:47 +0200

    add a string test suite in SimpleTest.elm

--- SimpleTest.elm
+++ SimpleTest.elm
@@ -4,14 +4,16 @@
 module Main exposing (..)
 
 import ElmTest exposing (..)
+import String
 
 
-tests : Test
-tests =
+listTests =
     suite "List Test Suite"
         [ test "List.Length" (assertEqual (List.length [ 1, 2, 3, 4 ]) 4)
         , test "List.isEmpty" <| assert (List.isEmpty [])
         , test "List.head" <| assertEqual (List.head [ 1, 2, 3 ]) (Just 1)
+          -- Failing:
+        , equals (List.tail [ 1, 2, 3, 4 ]) (Just [ 2, 3 ])
         , test "List.reverse" <|
             assertEqual (List.reverse [ 1, 2, 3 ]) [ 3, 2, 1 ]
         , test "Double reverse is identity" <|
@@ -23,6 +25,18 @@ tests =
         ]
 
 
+stringTests =
+    suite "String Test Suite"
+        [ defaultTest <| assert (String.isEmpty "")
+        , test "String.isEmpty" <| assert (String.isEmpty "")
+        , (String.reverse "ABC") `equals` "CBA"
+        ]
+
+
+tests =
+    suite "all Tests" [ listTests, stringTests ]
+
+
 main : Program Never
 main =
-    runSuiteHtml tests
+    runSuite tests
